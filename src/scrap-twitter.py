8ac5b02a3d528e7d9e4b2e34a62629ec42819357
Raphael F <raphaelf.ti@gmail.com>
2019-08-06 20:47:05 -0300

    Scrap
    Added delimiter

--- src/scrap-twitter.py
+++ src/scrap-twitter.py
@@ -2,8 +2,8 @@ import csv
 
 from utils import conn_api, add_profile, get_statuses_between_dates
 
-start_at = ''
-end_at = ''
+start_at = '16082018'
+end_at = '29102018'
 
 targets = [
     'jairbolsonaro', 'Haddad_Fernando', 'alvarodias_', 'CaboDaciolo', 'cirogomes',
@@ -23,17 +23,18 @@ for target in targets:
     csv_file = open(f'../data/{target.lower()}.csv', 'w+',
                     newline='', encoding='utf-8')
 
-    csv_writer = csv.writer(csv_file)
+    csv_writer = csv.writer(csv_file, delimiter=';', quotechar='"')
 
     csv_writer.writerow(['id', 'name', 'screen_name', 'location', 'url', 'description', 'protected', 'verified',
                          'followers_count', 'friends_count', 'favourites_count', 'statuses_count', 'created_at',
                          'profile_image_url'])
 
     add_profile(api, target, csv_writer)
+
     csv_file.close()
 
     csv_file = open(f'../data/{target.lower()}_election_tweets.csv', 'w+', newline='', encoding='utf-8')
-    writer = csv.writer(csv_file)
+    writer = csv.writer(csv_file, delimiter=';', quotechar='"')
     writer.writerow(["id", "created_at", "text"])
 
     get_statuses_between_dates(api, target, start_at, end_at, writer)
